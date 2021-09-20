723705da40929ac2f26067e8e0561227115848f0
Varun Muriyanat <varunmuriyanat@gmail.com>
2021-09-20 19:40:57 -0400

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -2,22 +2,35 @@
 
 using namespace std;
 
-void print_array(int[], int);
-void bubble_sort(int[], int);
-void selection_sort(int[], int);
-void insertion_sort(int[], int);
+void print_array(int [], int);
+void bubble_sort(int [], int);
+void selection_sort(int [], int);
+void insertion_sort(int [], int);
+void merge_sort(int [], int, int);
+void merge(int [], int, int, int);
+void quick_sort(int [], int, int);
+int partition(int [], int, int);
 
 int main()
 {
     int n = 4;
     int arr[n] = {7,4,5,2};
     
-    print_array(arr, n);
+    //print_array(arr, n);
     
     //bubble_sort(arr, n);
-    selection_sort(arr, n);
+    //selection_sort(arr, n);
+    //insertion_sort(arr, n);
+    
+    //cout << "merge sort: " << endl;
+    //merge_sort(arr, 0, 3); 
+
+    cout << "quick sort" << endl;
+    quick_sort(arr, 0, 3);
     
     print_array(arr, n);
+
+    cout << "sorted";
     return 0;
 }
 
@@ -63,6 +76,84 @@ void selection_sort(int a[], int n) {
 }
 
 void insertion_sort(int a[], int n) {
+    for(int i = 0; i < n; i++)
+    {
+       int temp = a[i];
+       int j = i;
+
+       while(temp < a[j-1] && j > 0)
+       {
+           a[j] = a[j-1];
+           j = j - 1;
+       }
+       a[j] = temp;
+    }
+
+    cout << "insertion sort: ";
+    print_array(a, n);
+}
+
+void merge(int a[], int start, int mid, int end) {
+    int p = start, q = mid + 1;
+    int arr[end-start+1], k = 0;
+
+    for(int i = start; i <= end; i++){
+        if(p > mid)
+            arr[k++] = a[q++];
+        else if(q > end)
+            arr[k++] = a[p++];
+        else if(a[p] < a[q])
+            arr[k++] = a[p++];
+        else
+            arr[k++] = a[q++];
+    }
+
+    for(int p = 0; p < k; p++) {
+        a[start++] = arr[p];
+    }
+
+    cout << "Merge Completed" << endl;
+}
+
+void merge_sort(int a[], int start, int end) {
+    cout << "Inside merge_sort" << endl;
+    int mid;
+    if(start < end) {
+        mid = (start + end) / 2;
+        merge_sort(a, start, mid);
+        merge_sort(a, mid+1, end);
+
+        merge(a, start, mid, end);
+    }
+}
+
+int partition(int a[], int start, int end) {
+    int i = start + 1;
+    int piv = a[start];
+
+    for(int j = start + 1; j <= end; j++) {
+        if(a[j] < piv) {
+            swap(a[i], a[j]);
+            i += 1;
+        }
+    }
+
+    swap(a[start], a[i-1]);
+    return i-1;
+}
+
+int rand_partition(int a[], int start, int end) {
+    int random = start + rand() % (end-start+1);
+    swap(a[random], a[start]);
+    return partition(a, start, end);
+}
+
+void quick_sort(int a[], int start, int end) {
+    if(start < end) {
+        int piv_pos = partition(a, start, end);
+        quick_sort(a, start, piv_pos-1);
+        quick_sort(a, piv_pos+1, end);
+    }
 }
 
 void print_array(int a[], int n) {
